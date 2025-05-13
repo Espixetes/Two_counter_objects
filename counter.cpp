05941ad7cd281abe1075735291093eee4b75983e
Vladimir Miroshnichenko <160275691+Espixetes@users.noreply.github.com>
2025-05-13 14:45:59 +0200

    Update counter.cpp

--- counter.cpp
+++ counter.cpp
@@ -1,22 +1,26 @@
 #include "counter.hpp"
 
 Counter::Counter(int startValue, QObject* parent)
-    : QObject(parent), m_count(startValue) {
+    : QObject(parent), m_count(startValue) 
+{
 }
 
-int Counter::Increment() {
+int Counter::Increment() 
+{
     m_count++;
     emit countChanged(m_count);
     return m_count;
 }
 
-int Counter::Decrement() {
+int Counter::Decrement() 
+{
     m_count--;
     emit countChanged(m_count);
     return m_count;
 }
 
-void Counter::setCount(int value) {
+void Counter::setCount(int value) 
+{
     if (m_count != value) {
         m_count = value;
         emit countChanged(m_count);
